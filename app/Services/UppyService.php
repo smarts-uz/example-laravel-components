3da6dba3fb34256a2d8c3bfcc039a71b77583727
arabic0513 <rustamyusupov230@gmail.com>
2023-07-06 12:44:34 +0500

    uppy

--- app/Services/UppyService.php
+++ app/Services/UppyService.php
@@ -12,16 +12,17 @@ class UppyService
 {
 
     /**
+     * Move File to Path in Config
+     *
      * @param $request
      * @return bool
      */
     public function uploadImage($request): bool
     {
         if ($request->hasFile('file')) {
-            $fileName = time() . '_' . $request->file->getClientOriginalName();
-            $filePath = $request->file('file')
-                ->move(public_path("storage/uploads/"), $fileName);
-
+            $file = $request->file('file');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->move(public_path(config('uppy.upload_file')), $fileName);
         }
         return is_callable($filePath);
     }
