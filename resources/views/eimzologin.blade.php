8ef37bb72178cb83da064de5175f1a38ea012232
arabic0513 <rustamyusupov230@gmail.com>
2023-07-07 09:00:19 +0500

    eimzologin blade

--- resources/views/eimzologin.blade.php
+++ resources/views/eimzologin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.css" rel="stylesheet">
-    <title>Uztelecom</title>
+    <title>Example App</title>
     @bukStyles
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
