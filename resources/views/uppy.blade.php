b8a6af0dd665c78eabd5ab0fbad10a9d580c0f89
arabic0513 <rustamyusupov230@gmail.com>
2023-07-10 14:16:50 +0500

    uppy

--- resources/views/uppy.blade.php
+++ resources/views/uppy.blade.php
@@ -1,5 +1,5 @@
 @extends('app')
 @section('center_content')
     <div id="file"></div>
-    <x-laravelUppy width="300" height="200" url="{{route('uploadImage')}}" target="#file" fieldName="file"></x-laravelUppy>
+    <x-SmartsUppy width="300" height="200" url="{{route('uploadImage')}}" target="#file" fieldName="file"></x-SmartsUppy>
 @endsection
