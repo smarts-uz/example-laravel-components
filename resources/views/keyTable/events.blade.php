a8c386bee5d15aa0fd5f3e87035c3615b99c5e20
arabic0513 <rustamyusupov230@gmail.com>
2023-09-23 18:05:44 +0500

    deleted css

--- resources/views/keyTable/events.blade.php
+++ resources/views/keyTable/events.blade.php
@@ -11,11 +11,6 @@
                 events.prepend(div);']" :options="['fixedHeader' => 'true']" :events="['key' => 'function (e, datatable, key, cell, originalEvent) {message(`Key press: ` + key + ` for cell <i>` + cell.data() + `</i>`);}','key-focus' => 'function (e, datatable, cell) {message(`Cell focus: <i>` + cell.data() + `</i>`);}','key-blur' => 'function (e, datatable, cell) {message(`Cell blur: <i>` + cell.data() + `</i>`);}']" getData="{{ route('report') }}" exportId="{{\App\Reports\One::class}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"></x-SmartsTable>
 @stop
 @section('css')
-    <style>
-        thead input {
-            width: 100%;
-        }
-    </style>
     @bukStyles(true)
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 @stop
