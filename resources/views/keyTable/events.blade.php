bddfdaf21ff04c339c837ea18f2480181c0066f6
arabic0513 <rustamyusupov230@gmail.com>
2023-09-24 12:27:41 +0500

    keyTable Events

--- resources/views/keyTable/events.blade.php
+++ resources/views/keyTable/events.blade.php
@@ -4,11 +4,11 @@
 @section('content_header')
 @stop
 @section('content')
-    <x-SmartsTable tableId="report" keys="true" :functions="['message' => 'let events = document.querySelector(`#reportText`);
+    <x-SmartsTable tableId="report" keys="true" :functions="['message' => ['parameters' => 'str','value' => 'let events = document.querySelector(`#reportText`);
                 let div = document.createElement(`div`);
 
                 div.innerHTML = str;
-                events.prepend(div);']" :options="['fixedHeader' => 'true']" :events="['key' => 'function (e, datatable, key, cell, originalEvent) {message(`Key press: ` + key + ` for cell <i>` + cell.data() + `</i>`);}','key-focus' => 'function (e, datatable, cell) {message(`Cell focus: <i>` + cell.data() + `</i>`);}','key-blur' => 'function (e, datatable, cell) {message(`Cell blur: <i>` + cell.data() + `</i>`);}']" getData="{{ route('report') }}" exportId="{{\App\Reports\One::class}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"></x-SmartsTable>
+                events.prepend(div);']]" :options="['fixedHeader' => 'true']" :events="['key' => 'function (e, datatable, key, cell, originalEvent) {message(`Key press: ` + key + ` for cell <i>` + cell.data() + `</i>`);}','key-focus' => 'function (e, datatable, cell) {message(`Cell focus: <i>` + cell.data() + `</i>`);}','key-blur' => 'function (e, datatable, cell) {message(`Cell blur: <i>` + cell.data() + `</i>`);}']" getData="{{ route('report') }}" exportId="{{\App\Reports\One::class}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"></x-SmartsTable>
 @stop
 @section('css')
     @bukStyles(true)
