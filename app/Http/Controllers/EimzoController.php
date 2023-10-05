84f9fb9e389466a8bc8fea95150f0c1023cfff47
arabic0513 <rustamyusupov230@gmail.com>
2023-10-05 15:31:28 +0500

    EimzoController Log

--- app/Http/Controllers/EimzoController.php
+++ app/Http/Controllers/EimzoController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class EimzoController extends Controller
 {
     public function view() : View
     {
+        Log::info('EimzoControllerView',['blade' => 'eimzologin']);
         return view('eimzologin');
     }
 }
