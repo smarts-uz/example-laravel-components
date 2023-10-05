73eeb0a60a07607cfaa85fabd323cc3d530fdc7d
arabic0513 <rustamyusupov230@gmail.com>
2023-10-05 15:58:50 +0500

    Editor Link

--- resources/views/editor/joinSelf.blade.php
+++ resources/views/editor/joinSelf.blade.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+
+{{--  https://editor.datatables.net/examples/advanced/joinSelf.html  --}}
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
