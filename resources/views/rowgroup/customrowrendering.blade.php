92a7f66f2b25680d64f548f4ec5851dcb3abb7d0
arabic0513 <rustamyusupov230@gmail.com>
2023-10-05 16:03:51 +0500

    RowGroup Link

--- resources/views/rowgroup/customrowrendering.blade.php
+++ resources/views/rowgroup/customrowrendering.blade.php
@@ -1,5 +1,5 @@
 @extends('adminlte::page')
-
+{{-- https://datatables.net/extensions/rowgroup/examples/initialisation/customRow.html --}}
 @section('title', 'Example App')
 @section('content_header')
 @stop
