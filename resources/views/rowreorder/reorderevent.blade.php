5ec68929b5a7b5bbab6945a045fd585caf427765
arabic0513 <rustamyusupov230@gmail.com>
2023-10-05 16:05:04 +0500

    RowReorder Link

--- resources/views/rowreorder/reorderevent.blade.php
+++ resources/views/rowreorder/reorderevent.blade.php
@@ -1,5 +1,5 @@
 @extends('adminlte::page')
-
+{{-- https://datatables.net/extensions/rowreorder/examples/initialisation/events.html --}}
 @section('title', 'Example App')
 @section('content_header')
 @stop
