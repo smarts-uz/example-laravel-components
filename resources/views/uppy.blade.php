66ed096a0b03133033a125a0434592bfe258cdd1
arabic0513 <rustamyusupov230@gmail.com>
2023-10-09 16:47:30 +0500

    uppy.blade.php

--- resources/views/uppy.blade.php
+++ resources/views/uppy.blade.php
@@ -6,7 +6,9 @@
 @section('content')
     @bukScripts(true)
     <div class="d-flex justify-content-center" id="file"></div>
+    <div class="d-flex justify-content-center" id="file1"></div>
     <x-SmartsUppy url="{{route('uploadImage')}}" target="#file" fieldName="file"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file"></x-SmartsUppy>
 @stop
 @section('css')
     @bukStyles(true)
