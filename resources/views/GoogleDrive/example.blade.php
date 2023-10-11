c372a8ca83b2348bee0fb1f51c0174f07d83836a
arabic0513 <rustamyusupov230@gmail.com>
2023-10-11 16:31:10 +0500

    GoogleDrive/example.blade.php

--- resources/views/GoogleDrive/example.blade.php
+++ resources/views/GoogleDrive/example.blade.php
@@ -9,14 +9,14 @@
     <div class="d-flex justify-content-center" id="file2"></div>
     <div class="d-flex justify-content-center" id="file3"></div>
     <div class="d-flex justify-content-center" id="file4"></div>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" googleDriveUrl="#" webcam="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" googleDriveUrl="#" audio="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" googleDriveUrl="#" screenCapture="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" googleDriveUrl="#" width="400" height="550"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" googleDriveUrl="#" disabled="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" googleDriveUrl="#" theme="dark"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" googleDriveUrl="#" locale="ru_RU"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" googleDriveUrl="#" locale="uz_UZ"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" webcam="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" audio="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" screenCapture="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" width="400" height="550"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" disabled="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" theme="dark"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" locale="ru_RU"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" googleDriveUrl="https://api2.transloadit.com/companion/drive/redirect" locale="uz_UZ"></x-SmartsUppy>
 @stop
 @section('css')
     @bukStyles(true)
