194863a2f5005f10524dc47fda57173aaea61a12
arabic0513 <rustamyusupov230@gmail.com>
2023-10-11 16:31:23 +0500

    Instagram/example.blade.php

--- resources/views/Instagram/example.blade.php
+++ resources/views/Instagram/example.blade.php
@@ -9,14 +9,14 @@
     <div class="d-flex justify-content-center" id="file2"></div>
     <div class="d-flex justify-content-center" id="file3"></div>
     <div class="d-flex justify-content-center" id="file4"></div>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" instagramUrl="#" webcam="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" instagramUrl="#" audio="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" instagramUrl="#" screenCapture="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" instagramUrl="#" width="400" height="550"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" instagramUrl="#" disabled="true"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" instagramUrl="#" theme="dark"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" instagramUrl="#" locale="ru_RU"></x-SmartsUppy>
-    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" instagramUrl="#" locale="uz_UZ"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" webcam="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file1" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" audio="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" screenCapture="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file2" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" width="400" height="550"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" disabled="true"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file3" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" theme="dark"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" locale="ru_RU"></x-SmartsUppy>
+    <x-SmartsUppy url="{{route('uploadImage')}}" target="#file4" fieldName="file" instagramUrl="https://api2.transloadit.com/companion/instagram/redirect" locale="uz_UZ"></x-SmartsUppy>
 @stop
 @section('css')
     @bukStyles(true)
